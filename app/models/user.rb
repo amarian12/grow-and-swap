6324226a140f7eb043de848d7b3625d415c10b1d
David White <whited75@gmail.com>
2015-06-29 18:43:01 -0600

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, format: { with: /\A[A-Za-z][A-Za-z0-9._-]{2,19}\z/ }
+  validates :password, length: { minimum: 8 }, allow_nil: true
 end
