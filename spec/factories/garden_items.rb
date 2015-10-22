b4ce1d0032f7c054e74dbfd5a12fe8c17c0a8614
David White <whited75@gmail.com>
2015-10-21 21:59:40 -0600

    Modify factories for new models and associations

--- spec/factories/garden_items.rb
+++ spec/factories/garden_items.rb
@@ -1,14 +1,7 @@
 FactoryGirl.define do
   factory :garden_item do
-    factory :garden_item_with_produce_items do
-      # factory_girl_rails '~>4.0' uses ignore method; '5.0' uses transient
-      ignore do
-        produce_items_count 5
-      end
-
-      after(:create) do |garden_item, evaluator|
-        create_list(:produce_item, evaluator.produce_items_count, garden_item: garden_item)
-      end
-    end
+    quantity 7
+    user
+    produce_item
   end
 end
