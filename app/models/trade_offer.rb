c39de75fc38daa5b3afb9bd2dd5ef60a5d36f4e1
David White <whited75@gmail.com>
2016-02-06 18:53:37 -0700

    Add TradeOffer validations and associations

--- app/models/trade_offer.rb
+++ app/models/trade_offer.rb
@@ -1,2 +1,12 @@
 class TradeOffer < ActiveRecord::Base
+validates :quantity, :accepted, presence: true
+
+belongs_to :buyer, class_name: "User", foreign_key: :user_id
+
+belongs_to :garden_item
+
+has_one :seller, class_name: "User", through: :garden_item
+
+validates_presence_of :buyer, class_name: "User"
+validates_presence_of :garden_item
 end
--- spec/models/trade_offer_spec.rb
+++ spec/models/trade_offer_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe TradeOffer, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it "is invalid without a quantity" do
+      trade_offer = TradeOffer.new(quantity: nil)
+      trade_offer.valid?
+      expect(trade_offer.errors[:quantity]).to include("can't be blank")
+    end
+
+    it "is invalid without an accepted value" do
+      trade_offer = TradeOffer.new(accepted: nil)
+      trade_offer.valid?
+      expect(trade_offer.errors[:accepted]).to include("can't be blank")
+    end
+
+    it "validates presence of user" do
+      is_expected.to validate_presence_of :user
+    end
+
+    it "validates presence of garden item" do
+      is_expected.to validate_presence_of :garden_item
+    end
+  end
+
+  describe "Associations" do
+    it "belongs to user" do
+      is_expected.to belong_to :user
+    end
+
+    it "belongs to garden item" do
+      is_expected.to belong_to :garden_item
+    end
+  end
 end
