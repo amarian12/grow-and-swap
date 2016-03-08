55ca51c246bec436b618550483a1977a59bca0bc
David White <whited75@gmail.com>
2016-03-07 19:24:51 -0700

    Add styling to page headers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   before_action :admin_user,      only: :destroy
 
   def index
-    @users = User.all.paginate(page: params[:page], per_page: 20)
+    @users = User.all.paginate(page: params[:page], per_page: 15)
   end
 
   def show
