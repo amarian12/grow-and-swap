dc93ffc22554b63e5f224c71f201ee67ff667a49
David White <whited75@gmail.com>
2016-04-19 22:53:03 -0600

    Fix welcome#index view tests

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "welcome/index.html.erb", :type => :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when not logged in" do
+    it "displays only one button" do
+      render
+
+      expect(rendered).to have_link("Browse garden items in your area")
+      expect(rendered).to_not have_link("My trade offers")
+      expect(rendered).to_not have_link("View produce items inventory")
+    end
+  end
 end
