482415b8733daa617cd08e6c3b7be538f13fbb94
David White <whited75@gmail.com>
2016-04-24 17:37:30 -0600

    Produce item feature tests

--- spec/features/produce_items_features_spec.rb
+++ spec/features/produce_items_features_spec.rb
@@ -1,49 +1,96 @@
 require 'rails_helper'
 
 RSpec.feature ProduceItem, :type => :feature do
-  let(:user) { user = create(:user) }
+  given(:user) { user = create(:user) }
+  given(:admin) { admin = create(:admin) }
 
-  let(:produce_item) do
+  given!(:produce_item) do
     produce_item = create(:produce_item)
   end
 
-  # let(:garden_item) do
-  #   garden_item = create(:garden_item)
-  #   GardenItem.find garden.id
-  # end
+  feature "Produce items listing page features" do
+    context "When logged in as admin user" do
+      background do
+        log_in_with(admin)
+        visit "/produce_items"
+      end
 
-  # let(:produce_item) do
-  #   produce_item = garden_item.produce_items.create(:produce_item)
-  #   ProduceItem.find produce_item.id
-  # end
+      scenario "Admin user can create a new produce item" do
+        click_link "New produce item"
+        # Cannot get the following line to test
+        # expect(page).to have_current_path(new_produce_item_path)
+        expect(page).to have_field "Category"
+        fill_in("Name", with: "carrot")
+        select("Vegetable", from: "Category")
+        click_button "Create produce item"
+        expect(page).to have_text "Produce item was successfully created."
+      end
+    end
 
-  # let(:produce_items) do
-  #   create(:garden_item_with_produce_items)
-  # end
+    context "When logged in as non-admin user" do
+      background do
+        log_in_with(user)
+        visit "/produce_items"
+      end
 
-  # let(:user_attributes) { attributes_for :user }
-  # let(:invalid_user_attributes) { attributes_for :user, first_name: "" }
-  # let(:new_user_attributes) { attributes_for :user, first_name: "David" }
+      scenario "User can click show to see individual produce item" do
+        click_link "Show"
+        expect(current_path).to eq "/produce_items/3"
+      end
 
-  # let(:two_users) do
-  #   two_users = create_pair(:user)
-  # end
+      scenario "User cannot see 'New produce item' button" do
+        expect(page).to_not have_link "New produce item"
+      end
+    end
+  end
 
-  # let(:admin) do
-  #   admin = create(:admin)
-  # end
+  feature "Specific produce item page features" do
+    context "When logged in as admin user" do
+      background do
+        log_in_with(admin)
+        visit "/produce_items"
+        click_link "Show"
+      end
 
-  scenario "Create produce item" do
-    visit "/"
-    log_in_with(user)
-    visit "/produce_items"
-    click_link "New Produce Item"
-    # Cannot get the following line to test
-    # expect(page).to have_current_path(new_produce_item_path)
-    expect(page).to have_field "Category"
-    fill_in("Name", with: "carrot")
-    select("Vegetable", from: "Category")
-    click_button "Create produce item"
-    expect(page).to have_text "Produce item was successfully created."
+      scenario "Admin user can edit produce item" do
+        click_link "Edit"
+        expect(current_path).to eq "/produce_items/5/edit"
+      end
+
+      scenario "Admin user can destroy produce item" do
+        click_link "Destroy"
+        expect(current_path).to eq(produce_items_path)
+        expect(page).to have_content "Produce item was successfully destroyed."
+      end
+    end
+
+    context "When logged in as non-admin user" do
+      given!(:garden_item) do
+        garden_item = create(:garden_item, produce_item_id: produce_item.id)
+      end
+
+      background do
+        log_in_with(user)
+        visit "/produce_items"
+        click_link "Show"
+      end
+
+      scenario "User cannot see 'Edit' or 'Destroy' links" do
+        expect(page).to_not have_link "Edit"
+        expect(page).to_not have_link "Destroy"
+      end
+
+      scenario "User can see a list of people with current produce item up for trade" do
+        expect(page).to have_content(produce_item.sellers.first.first_name)
+      end
+
+      scenario "User can click on other people's produce item as garden item" do
+        within_table("People who have this item") do
+          click_link "Show item"
+        end
+        expect(page).to have_text "Garden item:"
+        expect(page).to have_text "Quantity:"
+      end
+    end
   end
 end
