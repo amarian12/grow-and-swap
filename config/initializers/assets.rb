47c282f4d821fc0314fbeb09556c4d908ddee6cb
David White <whited75@gmail.com>
2016-06-08 22:19:12 -0600

    Remove scaffolds.css file and run assets:precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( sessions.js users.js welcome.js scaffolds.css sessions.css users.css welcome.css )
+Rails.application.config.assets.precompile += %w( sessions.js users.js welcome.js sessions.css users.css welcome.css )
