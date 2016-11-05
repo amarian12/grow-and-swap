3ba78a423a51967d82b9d1da0e6ee6de4f0aa01a
David White <whited75@gmail.com>
2016-11-04 21:54:37 -0600

    User cannot see edit/destroy links of other user's garden items

--- spec/features/garden_items_features_spec.rb
+++ spec/features/garden_items_features_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.feature GardenItem, :type => :feature do
   given(:user) { user = create(:user) }
 
-  given(:garden_item) do
+  given(:other_user) { other_user = create(:user) }
+
+  given!(:garden_item) do
     garden_item = create(:garden_item)
   end
 
@@ -11,23 +13,49 @@ RSpec.feature GardenItem, :type => :feature do
     produce_item = create(:produce_item, name: "Carrot")
   end
 
-  scenario "View list of garden items" do
-    visit "/"
-    click_link "Browse garden items in your area"
-    expect(page).to have_text "Garden Items in Your Area"
+  context "When logged in or not" do
+    scenario "view list of garden items" do
+      visit "/"
+      click_link "Browse garden items in your area"
+      expect(page).to have_text "Garden Items in Your Area"
+    end
   end
 
-  scenario "Create garden item" do
-    visit "/"
-    log_in_with(user)
-    visit "/garden_items"
-    click_link "New garden item"
-    # Cannot get the following line to test
-    # expect(page).to have_current_path(new_garden_item_path)
-    expect(page).to have_field "Quantity"
-    select "Carrot", from: :garden_item_produce_item_id, visible: false
-    fill_in("Quantity", with: 5)
-    click_button "Create garden item"
-    expect(page).to have_text "Garden item was successfully created."
+  context "When not logged in" do
+    scenario "guest cannot see garden item edit/destroy links" do
+      visit "/garden_items"
+      expect(page).to have_link "Show"
+      expect(page).to_not have_link "Edit"
+      expect(page).to_not have_link "Destroy"
+    end
+  end
+
+  context "When logged in" do
+    background do
+      log_in_with(garden_item.seller)
+    end
+
+    scenario "create garden item" do
+      visit "/"
+      visit "/garden_items"
+      click_link "New garden item"
+      # Cannot get the following line to test
+      # expect(page).to have_current_path(new_garden_item_path)
+      expect(page).to have_field "Quantity"
+      select "Carrot", from: :garden_item_produce_item_id, visible: false
+      fill_in("Quantity", with: 5)
+      click_button "Create garden item"
+      expect(page).to have_text "Garden item was successfully created."
+    end
+
+    scenario "only owner of garden item can see edit/destroy links" do
+      visit "/garden_items"
+      click_link "Log out"
+      log_in_with(other_user)
+      visit "/garden_items"
+      expect(page).to have_link "Show"
+      expect(page).to_not have_link "Edit"
+      expect(page).to_not have_link "Destroy"
+    end
   end
 end
